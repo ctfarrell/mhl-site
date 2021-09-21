e1aa4baf0bf7d536c7b6218b695d582710a2a12e
ctfarrell <chancerztf@gmail.com>
2021-09-20 21:33:35 -0500

    wrapping a new directory around the build file

--- moose-hide-landing/pages/index.js
+++ moose-hide-landing/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Moose Hide Landing Parks and Rec!</a>
         </h1>
 
         <p className={styles.description}>
