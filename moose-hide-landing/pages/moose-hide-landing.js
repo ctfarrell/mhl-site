591a5b41336420583ca128353def1634ddac703a
ctfarrell <chancerztf@gmail.com>
2021-09-23 21:29:16 -0500

    fixed the index page

--- moose-hide-landing/pages/moose-hide-landing.js
+++ moose-hide-landing/pages/moose-hide-landing.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import HomeOptions from '../modules/homeOptions'
 
 export default function Home() {
