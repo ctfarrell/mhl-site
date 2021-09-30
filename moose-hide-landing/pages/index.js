a481a120be513b83693c060560f354bd714ef9c1
ctfarrell <chancerztf@gmail.com>
2021-09-29 21:02:03 -0500

    added fixed header on homepage

--- moose-hide-landing/pages/index.js
+++ moose-hide-landing/pages/index.js
@@ -14,13 +14,17 @@ export default function Home() {
             <meta name="Your one stop shop for all your city recreation needs!" content="Moose Hide Landing Parks and Recreation" />
             <link rel="icon" href="/favicon.ico" />
             </Head>
-            <span className="flex-none h-2/5 w-screen md:h-screen object-contain bg-fancy-office bg-cover bg-no-repeat">
-                    <div className="container flex-col align-middle bg-white bg-opacity-70 z-10 py-40">
-                        <section className="flex justify-center pt-4 pb-2">
-                            <Image src="/leaf-icon1.png" alt="Leaf Icon" layout="fixed" width="25" height="25" className="object-none object-center"/>
+            <div className="fixed container flex flex-row border-b-2 border-gray-800 bg-white bg-opacity-100 shadow-sm">
+                <h1 className="text-2xl text-gray-800">MOOSE HIDE LANDING TOWN WEBSITE</h1>
+                <h1 className="text-2xl text-gray-800 place-self-end">Sept 29, 2021</h1>
+            </div>
+            <span className="flex flex-none place-items-center h-2/5 w-screen md:h-screen object-contain bg-fancy-office bg-cover bg-no-repeat">
+                    <div className="flex flex-col mx-auto align-items-center bg-white bg-opacity-70">
+                        <section className="flex justify-center pt-10 pb-2">
+                            <Image src="/leaf-icon1.png" alt="Leaf Icon" layout="fixed" width="50" height="50" className="object-none object-center"/>
                         </section>
-                            <h1 className="text-3xl text-center align-middle text-green-800">TOWN OF</h1>
-                            <h1 className="text-4xl font-bold text-center align-middle text-green-800">MOOSE HIDE LANDING</h1>
+                            <h1 className="text-5xl text-center align-middle text-green-800 py-5">TOWN OF</h1>
+                            <h1 className="text-5xl font-bold text-center align-middle text-green-800 pb-10 px-10">MOOSE HIDE LANDING</h1>
                     </div>
             </span>
             <span className="flex flex-none h-4/5 flex-col md:flex-row md:h-screen z-10">
