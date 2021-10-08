19e8986061ff6a84a307584ca5596a6f3fd2f4c2
ctfarrell <chancerztf@gmail.com>
2021-10-07 21:18:37 -0500

    fixed build issue

--- moose-hide-landing/modules/homeOptions.js
+++ moose-hide-landing/modules/homeOptions.js
@@ -2,11 +2,11 @@ import Image from 'next/image'
 
 export default function HomeOptions(props) {
     return(
-    <div class="flex flex-row place-items-center cursor-pointer group p-3">
+    <div className="flex flex-row place-items-center cursor-pointer group p-3">
         <div className="h-14 w-14">
             <Image src={props.src} alt="Icon" layout="responsive" width={props.width} height={props.height}/>
         </div>
-        <p class="text-4xl font-bold text-center align-middle text-green-800 group-hover:animate-bounce opacity-100 tracking-wide p-2">{props.optionName}</p>
+        <p className="text-4xl font-bold text-center align-middle text-green-800 group-hover:animate-bounce opacity-100 tracking-wide p-2">{props.optionName}</p>
     </div>
     )
 }
--- moose-hide-landing/pages/index.js
+++ moose-hide-landing/pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import HomeOptions from '../modules/HomeOptions'
-import OptionBubbles from '../modules/OptionBubbles'
+import HomeOptions from '../modules/HomeOptions';
+import OptionBubbles from '../modules/OptionBubbles';
 
 
 export default function Home() {
