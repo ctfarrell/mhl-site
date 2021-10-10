9900396730685196401c6338ffd81f7f873c2b44
ctfarrell <chancerztf@gmail.com>
2021-10-10 16:45:37 -0500

    attempt 1 at fixing image issues on Netlify

--- moose-hide-landing/next.config.js
+++ moose-hide-landing/next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  target: 'serverless',
   images: {
-    domains:["www.canva.com"],
+    domains: ['https://res.cloudinary.com/dsf7hsxxm/image/upload/v1633901610/mhl/'],
   },
   scripts: {
     domains:["www.form.jotform.com"],
