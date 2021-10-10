e8c1ae1e1787b4f121eaecccb19be6c8a89afb2d
ctfarrell <chancerztf@gmail.com>
2021-10-10 17:07:15 -0500

    might have a fix for the images

--- moose-hide-landing/next.config.js
+++ moose-hide-landing/next.config.js
@@ -2,9 +2,9 @@ module.exports = {
   reactStrictMode: true,
   target: 'serverless',
   images: {
-    domains: ['localhost'],
+    domains: ['localhost','https://res.cloudinary.com/dsf7hsxxm/image/upload/'],
     loader: "cloudinary",
-    path: "https://res.cloudinary.com/dsf7hsxxm/image/upload/v1633901610/mhl/",
+    path: "https://res.cloudinary.com/dsf7hsxxm/image/upload/",
   },
   scripts: {
     domains:["www.form.jotform.com"],
