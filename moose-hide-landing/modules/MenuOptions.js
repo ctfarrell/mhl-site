ae80189a2347936c65c33970440bae1ea61e7db3
ctfarrell <chancerztf@gmail.com>
2021-10-12 19:23:39 -0500

    defined the imageSource

--- moose-hide-landing/modules/MenuOptions.js
+++ moose-hide-landing/modules/MenuOptions.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 
+
 export default function HomeOptions(props) {
+    const imageSource = props.src
     return(
     <div className="flex flex-row place-items-center cursor-pointer group p-3">
         <div className="h-14 w-14">
-            <Image src={props.src} alt="Icon" layout="responsive" width={props.width} height={props.height}/>
+            <Image src={imageSource} alt="Icon" layout="responsive" width={props.width} height={props.height}/>
         </div>
         <p className="text-4xl font-bold text-center align-middle text-primary group-hover:animate-bounce opacity-100 tracking-wide p-2">{props.optionName}</p>
     </div>
