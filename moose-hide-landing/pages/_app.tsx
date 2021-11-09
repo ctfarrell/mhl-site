e3e717b59202c243ce8497271cbaba9fdd20b544
ctfarrell <chancerztf@gmail.com>
2021-11-08 20:57:52 -0600

    fixed build issue

--- moose-hide-landing/pages/_app.tsx
+++ moose-hide-landing/pages/_app.tsx
@@ -1,11 +1,35 @@
 import { AppProps } from 'next/app'
 import '../styles/globals.css'
 import { Web3ReactProvider, createWeb3ReactRoot } from '@web3-react/core'
-// import your favorite web3 convenience library here
+import { ethers } from "ethers";
+import Web3 from "web3";
+import Web3Modal from "web3modal";
 
-function getLibrary(provider) {
-  return new Web3Provider(provider) // this will vary according to whether you use e.g. ethers or web3.js
-}
+const providerOptions = {
+  /* See Provider Options Section */
+};
+
+const web3Modal = new Web3Modal({
+  network: "mainnet", // optional
+  cacheProvider: true, // optional
+  providerOptions // required
+});
+
+const provider = await web3Modal.connect();
+
+const web3 = new Web3(provider);
+// A Web3Provider wraps a standard Web3 provider, which is
+// what MetaMask injects as window.ethereum into each page
+//const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+// The MetaMask plugin also allows signing transactions to
+// send ether and pay to change state within the blockchain.
+// For this, you need the account signer...
+//const signer = provider.getSigner()
+
+//function getLibrary(provider) {
+//  return new Web3Provider(provider) // this will vary according to whether you use e.g. ethers or web3.js
+//}
 
 interface Web3ReactContextInterface<T = any> {
   activate: (
